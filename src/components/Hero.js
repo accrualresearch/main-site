5f6e5c618544e7079fd4247f5d48c0e13970120b
Joshua Barratt <30458569+jbarratt77@users.noreply.github.com>
2021-09-05 21:36:08 +0100

    :hammer: refactor hero components

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,29 +1,76 @@
 import * as React from "react"
 import "./Hero.css"
 
-const Hero = () => (
-  <div className="hero-container">
-    <div className="inner-container">
-      <div
-        className="hero-column hero-item"
-        style={{
-          flexBasis: "75%",
-        }}
-      >
-        <h2>Next Level Blockchain Products</h2>
-      </div>
-
-      <div className="hero-column">
-        <div className="hero-item">
-          <h2>Consulting</h2>
-        </div>
-
-        <div className="hero-item">
-          <h2>Data Driven Analysis</h2>
-        </div>
-      </div>
-    </div>
+const Container = ({ children, bg, text }) => (
+  <div className="hero-container" style={{
+    backgroundColor: bg,
+    color: text
+  }}>
+    <div className="inner-container">{children}</div>
   </div>
 )
 
-export default Hero
+export const Hero = () => (
+  <Container bg="#001657" text="white">
+    <div
+      className="hero-column hero-item"
+      style={{
+        flexBasis: "75%",
+      }}
+    >
+      <h2>Next Level Blockchain Products</h2>
+    </div>
+
+    <div
+      className="hero-column"
+      style={{
+        flexBasis: "25%",
+      }}
+    >
+      <div className="hero-item">
+        <h2>Consulting</h2>
+      </div>
+
+      <div className="hero-item">
+        <h2>Data Driven Analysis</h2>
+      </div>
+    </div>
+  </Container>
+)
+
+export const AboutHero = () => (
+  <Container bg="black" text="white">
+    <h2>About Us</h2>
+  </Container>
+)
+
+export const PulseBotHero = () => (
+  <Container bg="white" text="black">
+    <h2>PulseBot</h2>
+  </Container>
+)
+
+export const ChainPulseHero = () => (
+  <Container bg="black" text="white">
+    <h2>ChainPulse</h2>
+  </Container>
+)
+
+export const ConsultingHero = () => (
+  <Container bg="white" text="black">
+    <h2>Excellent Consulting</h2>
+  </Container>
+)
+
+export const CareersHero = () => (
+  <Container bg="black" text="white">
+    <h2>Careers</h2>
+  </Container>
+)
+
+export const ContactHero = () => (
+  <Container bg="white" text="black">
+    <h2>Contact</h2>
+  </Container>
+)
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,71 +2,19 @@ import * as React from "react"
 
 import Layout from "../components/Layout"
 import Seo from "../components/SEO"
-import Hero from "../components/Hero"
+import { Hero, AboutHero, PulseBotHero, ChainPulseHero, ConsultingHero, CareersHero, ContactHero } from "../components/Hero"
 
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
+    
     <Hero />
-    <div style={{
-      backgroundColor: "black",
-      width: "100%",
-      height: "200px",
-      color: "white",
-      display: "flex"
-    }}>
-      <h2>About Us</h2>
-    </div>
-
-    <div style={{
-      backgroundColor: "white",
-      width: "100%",
-      height: "200px",
-      color: "black",
-      display: "flex"
-    }}>
-      <h2>PulseBot</h2>
-    </div>
-
-    <div style={{
-      backgroundColor: "black",
-      width: "100%",
-      height: "200px",
-      color: "white",
-      display: "flex"
-    }}>
-      <h2>ChainPulse</h2>
-    </div>
-
-    <div style={{
-      backgroundColor: "white",
-      width: "100%",
-      height: "200px",
-      color: "black",
-      display: "flex"
-    }}>
-      <h2>Excellent Consulting</h2>
-    </div>
-
-    <div style={{
-      backgroundColor: "black",
-      width: "100%",
-      height: "200px",
-      color: "white",
-      display: "flex"
-    }}>
-      <h2>Careers</h2>
-    </div>
-
-    <div style={{
-      backgroundColor: "white",
-      width: "100%",
-      height: "200px",
-      color: "black",
-      display: "flex"
-    }}>
-      <h2>Contact</h2>
-    </div>
+    <AboutHero />
+    <PulseBotHero />
+    <ChainPulseHero />
+    <ConsultingHero />
+    <CareersHero />
+    <ContactHero />
 
   </Layout>
 )
