0c083f04534df66932502b96ca2680ff33fe6605
Joshua Barratt <30458569+jbarratt77@users.noreply.github.com>
2021-09-05 16:54:25 +0100

    :construction: header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,35 +1,46 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
+import logo from "../img/accrual_white.svg"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+const Header = ({ siteTitle }) => {
+  const styles = {
+    navLink: {
+      textDecoration: "none",
+      color: "white",
+      fontFamily: "Raleway, Arial",
+      padding: 5
+    }
+  }
+  return (
+    <header style={{
+      backgroundColor: "#001657",
+      display: "flex",
+      justifyContent: "center",
+      padding: "20px"
+    }}>
+      <div style={{
+        display: "flex",
+        width: "70vw",
+        justifyContent: "space-between",
+        alignItems: "center"
+      }}>
+        <h1 style={{ margin: 0 }}>
+          <Link to="/">
+            <img src={logo} width={200} alt={siteTitle}/>
+          </Link>
+        </h1>
+        <span>
+          <Link to="/about" style={styles.navLink}>About</Link>
+          <Link to="/products" style={styles.navLink}>Products</Link>
+          <Link to="/consulting" style={styles.navLink}>Consulting</Link>
+          <Link to="/careers" style={styles.navLink}>Careers</Link>
+          <Link to="/contact" style={styles.navLink}>Contact</Link>
+        </span>
+      </div>
+    </header>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -10,7 +10,8 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./Header"
-import "./Layout.css"
+//import "./Layout.css"
+import "./Layout2.css"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
