6997b35ba7dbc4dcc564b70883a9f87f18596399
Joshua Barratt <30458569+jbarratt77@users.noreply.github.com>
2021-09-05 22:19:16 +0100

    typography

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,36 +9,34 @@ const Footer = ({ siteTitle }) => (
     <div className="footer-container">
       <div id="footer-links-container">
         <div className="footer-links-col" style={{
-          flexBasis: "75%"
+          flexBasis: "25%"
         }}>
-          <h1 style={{ margin: 0 }}>
-            <Link to="/">
-              <img src={logo} width={200} alt={siteTitle} />
-            </Link>
-          </h1>
+          <Link to="/">
+            <img src={logo} width={200} alt={siteTitle} />
+          </Link>
         </div>
         <div className="footer-links-col">
-          <Link to="/about">About Us</Link>
+          <Link className="footer-link-header" to="/about">About Us</Link>
         </div>
         <div className="footer-links-col">
-          <Link to="/products">Product</Link>
+          <Link className="footer-link-header" to="/products">Product</Link>
 
 
           <Link to="/products/chainpulse">ChainPulse</Link>
           <Link to="/products/pulsebot">PulseBot</Link>
         </div>
         <div className="footer-links-col">
-          <Link to="/consulting">Consulting</Link>
+          <Link className="footer-link-header" to="/consulting">Consulting</Link>
 
         </div>
         <div className="footer-links-col">
-          <Link to="/careers">Careers</Link>
+          <Link className="footer-link-header" to="/careers">Careers</Link>
 
           <Link to="/careers/it-security">IT Security</Link>
           <Link to="/careers/software-developer">Software Developer</Link>
         </div>
         <div className="footer-links-col">
-          <Link>Links</Link>
+          <Link className="footer-link-header">Links</Link>
 
           <Link to="https://www.twitter.com/">Twitter</Link>
           <Link to="https://www.linkedin.com/">LinkedIn</Link>
---
+++ src/utils/typography.js
@@ -0,0 +1,10 @@
+import Typography from "typography"
+
+const typography = new Typography({
+  baseFontSize: "18px",
+  baseLineHeight: 1.666,
+  headerFontFamily: ["Raleway"],
+  bodyFontFamily: ["Raleway"],
+})
+
+export default typography
