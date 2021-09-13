9e1ae933916b31de0be02d3a354ffcd312248cfd
Joshua Barratt <30458569+jbarratt77@users.noreply.github.com>
2021-09-13 20:46:47 +0100

    :hammer: refactor css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,30 +16,16 @@ const Footer = ({ siteTitle }) => (
           </Link>
         </div>
         <div className="footer-links-col">
-          <Link className="footer-link-header" to="/about">About Us</Link>
+          <Link className="footer-link-header" to="/about">About Us →</Link>
         </div>
         <div className="footer-links-col">
-          <Link className="footer-link-header" to="/products">Product</Link>
-
-
-          <Link to="/products/chainpulse">ChainPulse →</Link>
-          <Link to="/products/pulsebot">PulseBot →</Link>
+          <Link className="footer-link-header" to="/products">Product →</Link>
         </div>
         <div className="footer-links-col">
-          <Link className="footer-link-header" to="/consulting">Consulting</Link>
-
+          <Link className="footer-link-header" to="/consulting">Consulting →</Link>
         </div>
         <div className="footer-links-col">
-          <Link className="footer-link-header" to="/careers">Careers</Link>
-
-          <Link to="/careers/it-security">IT Security →</Link>
-          <Link to="/careers/software-developer">Software Developer →</Link>
-        </div>
-        <div className="footer-links-col">
-          <Link className="footer-link-header">Links</Link>
-
-          <Link to="https://www.twitter.com/">Twitter →</Link>
-          <Link to="https://www.linkedin.com/">LinkedIn →</Link>
+          <Link className="footer-link-header" to="/careers">Careers →</Link>
         </div>
       </div>
       <p>© 2021, Accrual Research</p>
