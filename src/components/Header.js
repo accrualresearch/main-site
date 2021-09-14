89f521e8f22923aa9b205c06ebe392ae2a5d2662
Joshua Barratt <30458569+jbarratt77@users.noreply.github.com>
2021-09-15 00:38:28 +0100

    :sparkles: burger menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,45 +9,71 @@ import { AnchorLink } from "gatsby-plugin-anchor-links";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
 
-const Header = ({ siteTitle }) => (
-  <header className="header">
-    <nav className="header-container">
-      <Link to="/" style={{padding: "5px"}}>
-        <img src={logo} width={150} alt={siteTitle} />
-      </Link>
-      <ul>
-        <li>
-          <AnchorLink to="#about">
-            About
-          </AnchorLink>
-        </li>
-        <li>
-          <AnchorLink to="#products">
-            Products
-          </AnchorLink>
-        </li>
-        <li>
-          <AnchorLink to="#consulting">
-            Consulting
-          </AnchorLink>
-        </li>
-        <li>
-          <AnchorLink to="#careers">
-            Careers
-          </AnchorLink>
-        </li>
-        <li>
-          <AnchorLink to="#contact">
-            Contact
-          </AnchorLink>
-        </li>
-      </ul>
-    </nav>
-    <div className="mobile-header-container">
-      <FontAwesomeIcon icon={faBars} />
-    </div>
-  </header>
+const NavLinks = ({ handleOpen }) => (
+  <ul>
+    <li to="#about">
+      <AnchorLink to="#about" onAnchorLinkClick={handleOpen}>
+        About
+      </AnchorLink>
+    </li>
+    <li>
+      <AnchorLink to="#products">
+        Products
+      </AnchorLink>
+    </li>
+    <li>
+      <AnchorLink to="#consulting">
+        Consulting
+      </AnchorLink>
+    </li>
+    <li>
+      <AnchorLink to="#careers">
+        Careers
+      </AnchorLink>
+    </li>
+    <li>
+      <AnchorLink to="#contact">
+        Contact
+      </AnchorLink>
+    </li>
+  </ul>
 )
+
+const Header = ({ siteTitle }) => {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => {
+    setOpen(!open);
+  }
+  return (
+    <div className="main-container">
+      <header className="header container">
+        <nav className="inner">
+          <div className="site-logo">
+            <Link to="/">
+              <img src={logo} width={150} alt={siteTitle} />
+            </Link>
+          </div>
+          <div className="nav-links">
+            <NavLinks />
+          </div>
+          <div className="burger-icon" onClick={handleOpen}>
+            <FontAwesomeIcon icon={faBars} />
+          </div>
+        </nav>
+      </header>
+
+
+      <div className={`mobile-nav-overlay open-${open}`}>
+        <div className={`mobile-nav-column open-${open}`}>
+
+        </div>
+        <div className={`mobile-nav-column open-${open}`}>
+          <NavLinks handleOpen={handleOpen}/>
+        </div>
+      </div>
+    </div>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
