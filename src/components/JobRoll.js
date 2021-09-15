1f7dff9cbfdcce3b1016f8ae7145345d9970aa4b
Joshua Barratt <30458569+jbarratt77@users.noreply.github.com>
2021-09-15 01:24:25 +0100

    remove hyperlinks on jobroll

--- src/components/JobRoll.js
+++ src/components/JobRoll.js
@@ -16,18 +16,18 @@ class JobRoll extends React.Component {
               <article className="job-inner">
                 <header>
                   <h2>
-                    <Link className="job-link" to={post.fields.slug}>
+                    {/* <Link className="job-link" to={post.fields.slug}> */}
                       {post.frontmatter.title}
-                    </Link>
+                    {/* </Link> */}
                   </h2>
                 </header>
                 <p>
                   {post.excerpt}
-                  <br />
+                  {/* <br />
                   <br />
                   <Link className="job-link" to={post.fields.slug}>
                     →
-                  </Link>
+                  </Link> */}
                 </p>
               </article>
             </div>
