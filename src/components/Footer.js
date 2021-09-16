de90d05006071c60622b52d10868623b778319e6
Joshua Barratt <30458569+jbarratt77@users.noreply.github.com>
2021-09-16 23:18:27 +0100

    Show cookie popup

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,37 +5,48 @@ import logo from "../img/accrual.svg"
 import "./Footer.css"
 import { AnchorLink } from "gatsby-plugin-anchor-links";
 
-const Footer = ({ siteTitle }) => (
-  <footer>
-    <div className="footer-container">
-      <div id="footer-links-container">
-        <div className="footer-links-col" style={{
-          flexBasis: "25%"
-        }}>
-          <Link to="/">
-            <img src={logo} width={200} alt={siteTitle} />
-          </Link>
+const Footer = ({ siteTitle }) => {
+  function showCookiePrefs() {
+    if(!window.CookieThough) {
+      return;
+    }
+    window.CookieThough.show()
+  }
+  return (
+    <footer>
+      <div className="footer-container">
+        <div id="footer-links-container">
+          <div className="footer-links-col" style={{
+            flexBasis: "25%"
+          }}>
+            <Link to="/">
+              <img src={logo} width={200} alt={siteTitle} />
+            </Link>
+          </div>
+          <div className="footer-links-col">
+            <AnchorLink className="footer-link-header" to="/#about">About Us →</AnchorLink>
+          </div>
+          <div className="footer-links-col">
+            <AnchorLink className="footer-link-header" to="/#products">Product →</AnchorLink>
+          </div>
+          <div className="footer-links-col">
+            <AnchorLink className="footer-link-header" to="/#consulting">Consulting →</AnchorLink>
+          </div>
+          <div className="footer-links-col">
+            <AnchorLink className="footer-link-header" to="/#careers">Careers →</AnchorLink>
+          </div>
+          <div className="footer-links-col">
+            <AnchorLink className="footer-link-header" to="/#contact">Contact →</AnchorLink>
+          </div>
+          <div className="footer-links-col">
+            <a className="footer-link-header" style={{cursor: "pointer"}} onClick={showCookiePrefs}>Cookie Preferences →</a>
+          </div>
         </div>
-        <div className="footer-links-col">
-          <AnchorLink className="footer-link-header" to="/#about">About Us →</AnchorLink>
-        </div>
-        <div className="footer-links-col">
-          <AnchorLink className="footer-link-header" to="/#products">Product →</AnchorLink>
-        </div>
-        <div className="footer-links-col">
-          <AnchorLink className="footer-link-header" to="/#consulting">Consulting →</AnchorLink>
-        </div>
-        <div className="footer-links-col">
-          <AnchorLink className="footer-link-header" to="/#careers">Careers →</AnchorLink>
-        </div>
-        <div className="footer-links-col">
-          <AnchorLink className="footer-link-header" to="/#contact">Contact →</AnchorLink>
-        </div>
+        <p>© 2021, Accrual Research</p>
       </div>
-      <p>© 2021, Accrual Research</p>
-    </div>
-  </footer>
-)
+    </footer>
+  )
+}
 
 Footer.propTypes = {
   siteTitle: PropTypes.string,
