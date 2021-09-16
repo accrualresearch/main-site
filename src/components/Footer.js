e433e51df917c81504622c40963e998bacd7bfd6
Joshua Barratt <30458569+jbarratt77@users.noreply.github.com>
2021-09-16 23:53:45 +0100

    add social links and reorganise footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,8 @@ import { Link } from "gatsby"
 import logo from "../img/accrual.svg"
 import "./Footer.css"
 import { AnchorLink } from "gatsby-plugin-anchor-links";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTwitter, faLinkedin } from "@fortawesome/free-brands-svg-icons"
 
 const Footer = ({ siteTitle }) => {
   function showCookiePrefs() {
@@ -17,7 +19,7 @@ const Footer = ({ siteTitle }) => {
       <div className="footer-container">
         <div id="footer-links-container">
           <div className="footer-links-col" style={{
-            flexBasis: "25%"
+            flexBasis: "25%",
           }}>
             <Link to="/">
               <img src={logo} width={200} alt={siteTitle} />
@@ -25,21 +27,23 @@ const Footer = ({ siteTitle }) => {
           </div>
           <div className="footer-links-col">
             <AnchorLink className="footer-link-header" to="/#about">About Us →</AnchorLink>
-          </div>
-          <div className="footer-links-col">
             <AnchorLink className="footer-link-header" to="/#products">Product →</AnchorLink>
-          </div>
-          <div className="footer-links-col">
             <AnchorLink className="footer-link-header" to="/#consulting">Consulting →</AnchorLink>
           </div>
           <div className="footer-links-col">
             <AnchorLink className="footer-link-header" to="/#careers">Careers →</AnchorLink>
-          </div>
-          <div className="footer-links-col">
             <AnchorLink className="footer-link-header" to="/#contact">Contact →</AnchorLink>
           </div>
           <div className="footer-links-col">
             <a className="footer-link-header" style={{cursor: "pointer"}} onClick={showCookiePrefs}>Cookie Preferences →</a>
+          </div>
+          <div className="footer-links-col">
+            <Link className="footer-link-header" to="https://twitter.com/MrBenLilly">
+              <FontAwesomeIcon icon={faTwitter} /> Twitter
+            </Link>
+            <Link className="footer-link-header" to="https://twitter.com/MrBenLilly">
+              <FontAwesomeIcon icon={faLinkedin} /> LinkedIn
+            </Link>
           </div>
         </div>
         <p>© 2021, Accrual Research</p>
